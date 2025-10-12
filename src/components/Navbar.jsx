7047d6e90849a3d37ca251e52c4bf433cdfab771
Pablo Valladares González <PValladaresG@proton.me>
2025-10-12 17:48:11 +0200

    a little fix and Navbar components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <a href="/#">
+                    <img src="/logo.svg" alt="Apple logo" />
+                </a>
+
+                <ul>
+                    {navLinks.map(({ label }) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className='flex-center gap-3'>
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+
+                </div>
+            </nav>
+
+        </header>
+    );
+};
+
+export default Navbar;
